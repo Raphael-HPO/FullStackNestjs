f027e82d261f0354e7054e3182c03cbb0dfe51a5
Orati <r.orati@ativalog.com.br>
2026-03-12 15:06:13 -0300

    feat: ajuste no endpoint POST

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -7,7 +7,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @Post()
+  @Post('create')
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.userService.create(createUserDto);
   }
